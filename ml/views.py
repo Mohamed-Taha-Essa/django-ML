0d0b07666262dad523e72ea0f001cf17e6f825a9
Mohamed Abdallah Essa <www.taha.166@gmail.com>
2024-05-17 04:31:23 +0300

    show form

--- ml/views.py
+++ ml/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Iris
+from .forms import IrisForm
 # Create your views here.
+def predict(request):
+    form = IrisForm()
+
+
+    return render(request ,'ml/predict.html',{"form":form})
